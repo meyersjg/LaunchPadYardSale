e22e3149fc3dce6b6ad747c7e8b6cd3540072353
John Meyers <meyersjg@miamioh.edu>
2014-07-17 23:22:19 -0500

    retrying heroku

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,12 +1,5 @@
 class Order < ActiveRecord::Base
+
   belongs_to :product
 
-  # after_save :mark_product_as_ordered
-
-
-  # model stuff
-  # private
-  # def mark_product_as_ordered
-  #   self.product.update_attribute(:is_ordered, true)
-  # end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -19,13 +19,3 @@ Product.create(name: 'Ikea Alex', price: 160, image_url: 'https://s3-us-west-2.a
 Product.create(name: 'Ikea Alex', price: 160, image_url: 'https://s3-us-west-2.amazonaws.com/launchpadyardsale/alex.JPG')
 Product.create(name: 'Ikea Alex', price: 160, image_url: 'https://s3-us-west-2.amazonaws.com/launchpadyardsale/alex.JPG')
 Product.create(name: 'Ikea Alex', price: 160, image_url: 'https://s3-us-west-2.amazonaws.com/launchpadyardsale/alex.JPG')
-
-Order.create(product_id: '4', full_name: "Raghu Betina", email: "example@example.com", creditcard: '1234123412341234', expiration_date: "2014-07-09" )
-Order.create(product_id: '2', full_name: "Corey Jacobs", email: "example@example.com", creditcard: '1234123412341234', expiration_date: "2014-07-09" )
-Order.create(product_id: '4', full_name: "Jon Hussain", email: "example@example.com", creditcard: '1234123412341234', expiration_date: "2014-07-09" )
-Order.create(product_id: '4', full_name: "Will Rush", email: "example@example.com", creditcard: '1234123412341234', expiration_date: "2014-07-09" )
-Order.create(product_id: '4', full_name: "Elise Haupt", email: "example@example.com", creditcard: '1234123412341234', expiration_date: "2014-07-09" )
-Order.create(product_id: '4', full_name: "Jack McCallum", email: "example@example.com", creditcard: '1234123412341234', expiration_date: "2014-07-09" )
-Order.create(product_id: '4', full_name: "Emily Meyers", email: "example@example.com", creditcard: '1234123412341234', expiration_date: "2014-07-09" )
-Order.create(product_id: '4', full_name: "John Meyers", email: "example@example.com", creditcard: '1234123412341234', expiration_date: "2014-07-09" )
-Order.create(product_id: '4', full_name: "Maren Meyers", email: "example@example.com", creditcard: '1234123412341234', expiration_date: "2014-07-09" )
