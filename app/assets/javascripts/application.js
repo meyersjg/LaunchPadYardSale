e33ae83b3188c7711967a550ed57d8806ece1c1d
John Meyers <meyersjg@miamioh.edu>
2014-07-17 23:01:20 -0500

    removed validation gem

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -35,49 +35,5 @@ $(document).ready(function(){
     $('html, body').animate({scrollTop : 0},800);
     return false;
   });
-
-  //Order form validations
-  (function($,W,D)
-  {
-      var JQUERY4U = {};
-
-      JQUERY4U.UTIL =
-      {
-          setupFormValidation: function()
-          {
-              //form validation rules
-              $(".newOrderForm").validate({
-                  rules: {
-                      email: {
-                          required: true,
-                          email: true
-                      },
-                      creditcard: {
-                          required: true,
-                          minlength: 16
-                      },
-                      agree: "required"
-                  },
-                  messages: {
-                      creditcard: {
-                          required: "Please provide a valid credit card",
-                          minlength: "Your credit card number must be at least 16 characters long"
-                      },
-                      email: "Please enter a valid email address",
-                      agree: "Please accept our policy"
-                  },
-                  submitHandler: function(form) {
-                      form.submit();
-                  }
-              });
-          }
-      }
-
-      //when the dom has loaded setup form validation rules
-      $(D).ready(function($) {
-          JQUERY4U.UTIL.setupFormValidation();
-      });
-
-  })(jQuery, window, document);
 });
 
